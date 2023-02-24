2c42897ea9b70bac6092b2346793c56caed4c547
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-23 18:09:55 -0800

    linked generateMarkdown page to index.js and fixed other bugs

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,7 @@
 function renderLicenseBadge(license) {
   if (license !== "none"){
-    return `https://img.shields.io/badge/license-${license}-brightgreen`;
+    return `https://img.shields.io/badge/license-${license}-brightgreen
+    `;
 
   }
   return "";
@@ -25,22 +26,34 @@ Licensed under the ${license} license.`
 function generateMarkdown(data) {
   return `
 # ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licenses](#licenses)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+
 ${renderLicenseBadge(data.license)}
 
 #License
 ${renderLicenseLink(data.license)}
-
-## Description
-${data.description}
 
 ## Installation
 ${data.installation}
 
 ## Usage
 ${data.usage}
-## Credit
 
+## Credit
 ${data.credit}
+
 ## license
-![License](https://img.shields.io/static/v1?label=License&message="${badge}&color=GREEN)` 
+![License](https://img.shields.io/static/v1?label=License&message="${data.license}&color=GREEN)` 
 }
+
+module.exports = generateMarkdown
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
-const path = require("path");
+//const path = require("path");
 const generateMarkdown = require("./generateMarkdown");
 
 const questions = [
@@ -31,21 +31,19 @@ const questions = [
         message: 'Who are the contributers to this project?',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
         message: "Please select a licesne applicable to this project.",
         choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
     },
 ];
 
-function writeToFile (fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
 
   function init() {
     inquirer.prompt(questions).then((responses) =>{
-      console.log("Creating README.md");
-      writeToFile("./distribution/README.md", generateMarkdown({...responses }));
+      fs.writeFile("./distribution/README.md",generateMarkdown({...responses }), (error)=>{
+        error ? console.error(error) : console.log("Creating README.md")
+      })
     });
   }
 
